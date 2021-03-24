7d076ac7fb5160cc70effba0258fb6d32853b23e
Astronomdef <78546167+Astronomdef@users.noreply.github.com>
2021-03-24 14:29:39 +0200

    install_ev3dev.js
    Это файл который запускает установку ev3dev на ваш ev3!

---
+++ install_ev3dev.js
@@ -0,0 +1,10 @@
+npm install ev3dev
+pip install ev3dev
+download_repository pypi.org/module/ev3dev
+start install.cmd
+npm install discord.js
+copy install.js to EV3
+start install.js
+copy install.cmd
+copy js files in ev3dev.organd copy to EV3
+in EV3 npm install ev3.js
