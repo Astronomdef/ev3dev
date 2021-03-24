65c572a42a2dda9ce6a6bcc9c25d0bf3dbfbf0a6
Astronomdef <78546167+Astronomdef@users.noreply.github.com>
2021-03-24 14:59:39 +0200

    Update install_ev3dev.js

--- install_ev3dev.js
+++ install_ev3dev.js
@@ -1,12 +1,21 @@
-npm install ev3dev
-pip install ev3dev
-download_repository pypi.org/module/ev3dev
-start install.cmd
-npm install discord.js
-copy install.js to EV3
-start install.js
-copy install.cmd
-copy js files in ev3dev.organd copy to EV3
-in EV3 npm install ev3.js
+npm install ev3dev;
+pip install ev3dev;
+download_repository pypi.org/module/ev3dev;
+start install.cmd;
+npm install discord.js;
+copy install.js to EV3;
+start install.js;
+copy install.cmd;
+copy js files in ev3dev.organd copy to EV3;
+in EV3 npm install ev3.js;
 git@github.com:Astronomdef/ev3dev.git
 gh repo clone Astronomdef/ev3dev
+git@github.com:Astronomdef/ev3dev.git
+print('install new version?');
+If (text==yes);
+clone_repository_ev3dev.js;
+git@github.com:Astronomdef/ev3dev.js.git
+  }
+else
+  start install.py
+}
