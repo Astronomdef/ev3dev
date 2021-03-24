45e65d79433377f8fc92bcb66c1a3eb6aa911f17
Astronomdef <78546167+Astronomdef@users.noreply.github.com>
2021-03-24 14:38:22 +0200

    install.py
    Этот файл надо скопировать на ев3 и запустить его там просто на ПК запустите этот файл.

---
+++ install.py
@@ -0,0 +1,10 @@
+copy to EV3
+start install.js
+pip install ev3dev
+clone_github_repository ev3_python in EV3
+move python.exe to ev3python.exe
+download_repository_nodejssetup
+download nodejs.exe
+start nodejs.exe
+install in EV3 /s /d /0 /v /LTS /mode /sleep
+npm install ev3dev.js
